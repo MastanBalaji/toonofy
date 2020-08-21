ab48004ae64ea07252ad4a8a6119264f7e21dcf0
MastanBalaji <mastanbalaji@gmail.com>
2020-08-21 09:22:45 +0530

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,87 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'toonofy';
+
+  features=[
+    {
+    image:'assets/pic1.svg',
+    name:'SYLLABUS'
+  },
+  {
+    image:'assets/pic2.svg',
+    name:'CLASS'
+  },
+  {
+    image:'assets/pic3.svg',
+    name:'OLYMPIADS'
+  },
+  {
+    image:'assets/pic4.svg',
+    name:'SUBJECTS'
+  },
+  {
+    image:'assets/pic5.svg',
+    name:'WORKSHEETS'
+  },
+  {
+    image:'assets/pic6.svg',
+    name:'POSTERS'
+  },
+  {
+    image:'assets/pic7.svg',
+    name:'PICTURE TALK'
+  },
+  {
+    image:'assets/pic18.svg',
+    name:'GAMES'
+  },
+  {
+    image:'assets/pic9.svg',
+    name:'PUZZLES'
+  },
+  {
+    image:'assets/pic10.svg',
+    name:'PRACTISE'
+  },
+  {
+    image:'assets/pic11.svg',
+    name:'STORY BOOKS'
+  },
+  {
+    image:'assets/pic12.svg',
+    name:'MOCK TEST'
+  },
+  {
+    image:'assets/pic13.svg',
+    name:'WORKBOOKS'
+  },
+  {
+    image:'assets/pic14.svg',
+    name:'NCERT'
+  },
+  {
+    image:'assets/pic15.svg',
+    name:'REPORTS'
+  },
+  {
+    image:'assets/pic16.svg',
+    name:'REMEDIAL PLAN'
+  },
+  {
+    image:'assets/pic17.svg',
+    name:'MCQs'
+  },
+  {
+    image:'assets/pic18.svg',
+    name:'OLYMPIAD BOOKS'
+  },
+  {
+    image:'assets/pic19.svg',
+    name:'FLASHCARDS'
+  },
+  {
+    image:'assets/pic20.svg',
+    name:'VIDEOS'
+  },
+]
 }
